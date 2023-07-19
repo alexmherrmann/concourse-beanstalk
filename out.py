b98f4c804aea9d03f3cdfcf22ba590544ae94bbe
Alex Herrmann <alexander.m.herrmann@gmail.com>
2023-07-19 15:07:46 -0600

    aws auth

--- out.py
+++ out.py
@@ -4,24 +4,31 @@ import json
 import sys
 import boto3
 
-# Set up the client with the credentials
-client = boto3.client('elasticbeanstalk')
+
 # Create the application version
-def create_application_version(application_name, version_label, s3_bucket, s3_key):
+def create_application_version(
+    client, application_name, version_label, s3_bucket, s3_key
+):
     client.create_application_version(
         ApplicationName=application_name,
         VersionLabel=version_label,
-        SourceBundle={
-            'S3Bucket': s3_bucket,
-            'S3Key': s3_key
-        }
+        SourceBundle={"S3Bucket": s3_bucket, "S3Key": s3_key},
     )
 
+
 if __name__ == "__main__":
-    parsed = json.loads(sys.stdin.read())
+    stdin = sys.stdin.read()
+    parsed = json.loads(stdin)
+    # Print stdin to stderr
+    print(stdin, file=sys.stderr)
+    client = boto3.client(
+        "elasticbeanstalk",
+        aws_access_key_id=parsed["aws_access_key_id"],
+        aws_secret_access_key=parsed["aws_secret_access_key"],
+    )
     create_application_version(
-        parsed['application_name'], 
-        parsed['version_label'], 
-        parsed['s3_bucket'],
-        parsed['s3_key']
+        parsed["application_name"],
+        parsed["version_label"],
+        parsed["s3_bucket"],
+        parsed["s3_key"],
     )
