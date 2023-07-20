1361008fbfb013f7b7ae465dbfd1f9d15e270e03
Alex Herrmann <alexander.m.herrmann@gmail.com>
2023-07-20 10:31:17 -0600

    An in.py

---
+++ in.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# write "doing nothing" to stderr
+import sys
+import json
+
+
+print("doing nothing", file=sys.stderr)
+
+# load stdin as json
+stdin = json.loads(sys.stdin.read())
+print("{}")
