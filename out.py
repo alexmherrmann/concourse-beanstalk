41a8e9f51004770d4e82f5b92efadd8c8499588c
Alex Herrmann <alexander.m.herrmann@gmail.com>
2023-07-20 11:29:29 -0600

    Printing!

--- out.py
+++ out.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import glob
 import json
 import os
 import sys
@@ -32,14 +33,14 @@ if __name__ == "__main__":
     os.chdir(sys.argv[1])
 
     # recurse through all directories in the current directory, printing them to stderr
-    for root, dirs, files in os.walk("."):
-        # Print the file as a full path
-        for file in files:
-            content = open(os.path.join(root, file)).read()
-            print(f"{os.path.join(root, file)}: {content}", file=sys.stderr)
-        # Print the directory as a full path
-        for dir in dirs:
-            print(os.path.join(root, dir), file=sys.stderr)
+    # for root, dirs, files in os.walk("."):
+    #     # Print the file as a full path
+    #     for file in files:
+    #         content = open(os.path.join(root, file)).read()
+    #         print(f"{os.path.join(root, file)}: {content}", file=sys.stderr)
+    #     # Print the directory as a full path
+    #     for dir in dirs:
+    #         print(os.path.join(root, dir), file=sys.stderr)
         
     client = boto3.client(
         "elasticbeanstalk",
@@ -47,10 +48,30 @@ if __name__ == "__main__":
         aws_access_key_id=parsed["aws_access_key_id"],
         aws_secret_access_key=parsed["aws_secret_access_key"],
     )
-    create_application_version(
-        client,
-        parsed["application_name"],
-        parsed["version_label"],
-        parsed["s3_bucket"],
-        parsed["s3_key"],
-    )
+
+    # Get the version from the first "version" file we see in a subdirectory using a glob
+    version = open(glob.glob("*/version", recursive=False)[0]).read().strip()
+    
+    
+    # Get the s3 url from the first "url" file we see in a subdirectory using a glob
+    s3url = open(glob.glob("*/url", recursive=False)[0]).read().strip()
+
+    # Strip the https:// from the s3 url
+    s3url = s3url.strip("https://")
+    # Cut the first two directories from the s3 url, which is domain and the bucket name
+    s3key = "/".join(s3url.split("/")[2:])
+
+
+
+    # Print out our version label and s3 key
+    print(f"version: {version} s3key: {s3key}", file=sys.stderr)
+    
+    print(json.dumps({"version": {"ref": f"msai-{version}"}}))
+    # Create the application version
+    # create_application_version(
+    #     client,
+    #     parsed["application_name"],
+    #     version,
+    #     parsed["s3_bucket"],
+    #     s3_key=s3key,
+    # )
